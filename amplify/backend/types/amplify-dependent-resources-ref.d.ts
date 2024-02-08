483dfe1a9d2ad1e612fe75deab1415afaec608bf
ngand000 <ngand000@uw.edu>
2024-02-07 18:40:43 -0800

    merge

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3nicebreakersstoragec9bdab74": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
