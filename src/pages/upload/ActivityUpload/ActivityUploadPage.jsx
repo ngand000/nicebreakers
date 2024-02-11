325f2bf25637ed4c9fce7c1de3ba746b3fc45509
shivd <shivd04@cs.washington.edu>
2024-02-11 11:59:14 -0800

    dynamic upload button on view pages + working on upload pages

--- src/pages/upload/ActivityUpload/ActivityUploadPage.jsx
+++ src/pages/upload/ActivityUpload/ActivityUploadPage.jsx
@@ -1,35 +1,64 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+import UploadButton from "../UploadButton.jsx";
+import '../UploadPages.css';
 
 const UploadPage = (props) => {
+
+    const [textboxPlaceHolder, questionFocusToggler] = useState("Enter your Description here!");
+
+    function togglePlaceHolder() {
+        if (textboxPlaceHolder == "") {
+            questionFocusToggler("Enter your Description here!");
+        } else {
+            questionFocusToggler("");
+        }
+    }
+
     return (
         <div>
-            <form id ="uploadActivity">
-                <label for={"activityName"}>Activity Name:</label>
-                <input type={"text"} id={"activity"}/>
+            <div className="header">
+                <img src={"logoplaceholder.png"} alt={"logo"}/>
+                <text className="title">Upload Activity</text>
+            </div>
+            <form id ="uploadActivity" style={{width: "80%", marginLeft: "10%"}}>
+                <div className="name-entry-block">
+                    <label className="feild-entry-title" for={"activityName"}>Activity Name:</label>
+                    <input className="name-entry-box" type={"text"} id={"activity"}/>
+                </div>
                 <br/>
-                <label htmlFor={"activityDescription"}>Description:</label>
-                <textarea name={"description"} form={"uploadActivity"}>Enter activity description here.</textarea>
+                <div className="color-box" style={{backgroundColor: "rgb(255,252,123)"}}>
+                    <label className="feild-entry-title" htmlFor={"activityDescription"}>Description:</label>
+                    <textarea id="question-entry-box" className="question-entry-box" style={{width: "95%", height: "20vh"}} name={"Description"} form={"uploadActivity"} placeholder={textboxPlaceHolder} onFocus={togglePlaceHolder} onBlur={togglePlaceHolder}></textarea>
+                </div>
                 <br/>
                 {/*preload and lock this with username passed in*/}
-                <label htmlFor={"authorName"}>Author:</label>
-                <input type={"text"} id={"author"}/>
+                <div className="color-box" style={{backgroundColor: "rgb(182,255,123)"}}>
+                    <label className="feild-entry-title" htmlFor={"authorName"}>Author:</label>
+                    <input className="author-field" type={"text"} id={"author"}/>
+                </div>
                 <br/>
-                <label htmlFor={"playerCount"}>Player Count:</label>
-                <input type="number" min="1" max="100" id={"playerCount"}/>
+                <div className="color-box" style={{backgroundColor: "rgb(215, 109, 236)"}}>
+                    <label className= "feild-entry-title" htmlFor={"playerCount"}>Player Count:</label>
+                    <input style={{padding: "1vh 1vw 1vh 1vw", fontSize: "3vmin"}} type="number" min="1" max="100" id={"playerCount"}/>
+                </div>
                 <br/>
                 {/*needs to be duration*/}
                 <label htmlFor={"duration"}>Duration:</label>
                 <input type="time" id={"duration"}/>
                 <br/>
-                <label htmlFor={"ageRange"}>Age Range:</label>
-                <input type="number" min="1" max="100" id={"minAge"}/>
-                to
-                <input type="number" min="1" max="100" id={"maxAge"}/>
+                <div className="color-box" style={{backgroundColor: "rgb(255,94,94)"}}>
+                    <label className="feild-entry-title" style={{marginLeft: "2vw"}} htmlFor={"ageRange"}>Age Range:</label>
+                    <ul style={{margin: "2vh 0 2vh 2vw", padding: "0", display: "flex", listStyleType: "none"}}>
+                        <li><div className="color-box" style={{backgroundColor: "rgb(255,187,89)", display: "inline-block"}}><input style={{padding: "1vh 1vw 1vh 1vw", fontSize: "3vmin"}} type="number" min="1" max="100" id={"minAge"}/></div></li>
+                        <li><div className="feild-entry-title" style={{display: "inline-block", paddingTop: "5vh"}}>to</div></li>
+                        <li><div className="color-box" style={{backgroundColor: "rgb(182,255,123)", display: "inline-block"}}><input style={{padding: "1vh 1vw 1vh 1vw", fontSize: "3vmin"}} type="number" min="1" max="100" id={"maxAge"}/></div></li>
+                    </ul>
+                </div>
                 <br/>
                 <label htmlFor={"photos"}>Upload photos:</label>
                 <input type="file" id="activityPics" name="activityPics"/>
                 <br/>
-                <input type="submit"/>
+                <UploadButton uploadType={"/"}></UploadButton>
             </form>
             {/*<div> Data entry/upload </div>*/}
             {/*<div> Button to upload </div>*/}
--- src/pages/upload/QuestionUpload/QuestionUploadPage.jsx
+++ src/pages/upload/QuestionUpload/QuestionUploadPage.jsx
@@ -1,24 +1,44 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+import UploadButton from "../UploadButton.jsx";
+import '../UploadPages.css';
 
 const UploadPage = (props) => {
+    
+    const [textboxPlaceHolder, questionFocusToggler] = useState("Enter your Question here!");
+
+    function togglePlaceHolder() {
+        if (textboxPlaceHolder == "") {
+            questionFocusToggler("Enter your Question here!");
+        } else {
+            questionFocusToggler("");
+        }
+    }
+
     return (
         <div>
-            <form id ="uploadQuestion">
-                <label for={"questionName"}>Question Name:</label>
-                <input type={"text"} id={"question"}/>
-                <br/>
-                <textarea name={"Question"} form={"uploadQuestion"}>Enter the Question here.</textarea>
+            <div className="header">
+                <img src={"logoplaceholder.png"} alt={"logo"}/>
+                <text className="title">Upload Question</text>
+            </div>
+            <form id ="uploadQuestion" style={{width: "80%", marginLeft: "10%"}}>
+                <div className="color-box" style={{backgroundColor: "rgb(255,252,123)"}}><textarea id="question-entry-box" className="question-entry-box" name={"Question"} form={"uploadQuestion"} placeholder={textboxPlaceHolder} onFocus={togglePlaceHolder} onBlur={togglePlaceHolder}></textarea></div>
                 <br/>
                 {/*preload and lock this with username passed in*/}
-                <label htmlFor={"authorName"}>Author:</label>
-                <input type={"text"} id={"author"}/>
+                <div className="color-box" style={{backgroundColor: "rgb(182,255,123)"}}>
+                    <label className="feild-entry-title" htmlFor={"authorName"}>Author:</label>
+                    <input className="author-field" type={"text"} id={"author"}/>
+                </div>
                 <br/>
-                <label htmlFor={"ageRange"}>Age Range:</label>
-                <input type="number" min="1" max="100" id={"minAge"}/>
-                to
-                <input type="number" min="1" max="100" id={"maxAge"}/>
+                <div className="color-box" style={{backgroundColor: "rgb(255,94,94)"}}>
+                    <label className="feild-entry-title" style={{marginLeft: "2vw"}} htmlFor={"ageRange"}>Age Range:</label>
+                    <ul style={{margin: "2vh 0 2vh 2vw", padding: "0", display: "flex", listStyleType: "none"}}>
+                        <li><div className="color-box" style={{backgroundColor: "rgb(255,187,89)", display: "inline-block"}}><input style={{padding: "1vh 1vw 1vh 1vw", fontSize: "3vmin"}} type="number" min="1" max="100" id={"minAge"}/></div></li>
+                        <li><div className="feild-entry-title" style={{display: "inline-block", paddingTop: "5vh"}}>to</div></li>
+                        <li><div className="color-box" style={{backgroundColor: "rgb(182,255,123)", display: "inline-block"}}><input style={{padding: "1vh 1vw 1vh 1vw", fontSize: "3vmin"}} type="number" min="1" max="100" id={"maxAge"}/></div></li>
+                    </ul>
+                </div>
                 <br/>
-                <input type="submit"/>
+                <UploadButton uploadType={"/questions"}></UploadButton>
             </form>
             {/*<div> Data entry/upload </div>*/}
             {/*<div> Button to upload </div>*/}
