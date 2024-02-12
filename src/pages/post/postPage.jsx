3c2ce5dc598d51fd9bc84f08430c60552b7df75b
mmontelaro <135753734+mmontelaro@users.noreply.github.com>
2024-02-11 18:11:26 -0800

    Updated some style stuff on post page

--- src/pages/post/postPage.jsx
+++ src/pages/post/postPage.jsx
@@ -2,7 +2,7 @@ import {useSearchParams} from "react-router-dom";
 import { Amplify } from 'aws-amplify';
 import { Activity } from '../../models';
 import config from '../../aws-exports.js';
-import {useEffect, useState} from "react";
+import {useEffect, useState, React} from "react";
 import {DataStore} from "aws-amplify/datastore";
 
 Amplify.configure(config);
@@ -17,21 +17,53 @@ const PostPage = () => {
 
     useEffect(() => {
         (async () => {
-        setActivity((await DataStore.query(Activity, (a) => a.and(a => [a.id.eq(postParams.get('id'))])))[0])
-        console.log(activity)})()
+        setActivity((await DataStore.query(Activity, (a) => a.and(a => [a.id.eq(postParams.get('id'))])))[0])})()
     })
 
+    const headerStyle = {height: "16vmin", display: "flex", margin: "auto", width: "90vw", justifyContent: "center", alignContent: "center"}
+
+    const titleAndValueStyle = {display: "flex", borderWidth: 5, border: "solid", borderRadius: "10px", flexShrink: 1, margin: "20px 5px 5px 5px", width: "20%"}
+
+    const titleStyle = {margin: "auto 1vmin auto 0"}
+
+    const valueStyle = {margin: "auto 0 auto 0"}
+
+    const attributesStyle = {display: "flex"}
+
+    const descStyle = {borderWidth: 5, border: "solid", borderRadius: "10px", borderColor: "rgb(150, 150, 150)", margin: "0 5px 0 5px"}
 
     return (<div>
             {activity && (<div>
-                <h1>{activity.name}</h1>
-            <p>Author: {activity.author}</p>
-            <p>Duration: {rangeToString(activity.duration)} {activity.duration === [1,1] ? "minute" : "minutes"}</p>
-            <p>Group Size: {rangeToString(activity.playerCount)} people</p>
-            <p>Age Range: {rangeToString(activity.ageRange)} years</p>
-            <h2>Description:</h2>
-            <p>{activity.description}</p>
-            <p>Likes: {activity.likes}</p>
+                <div style={headerStyle}>
+                    <img src={"logoplaceholder.png"} alt={"logo"}/>
+                    <h1>{activity.name}</h1>
+                </div>
+                <div style={attributesStyle}>
+                    <div style={titleAndValueStyle}>
+                        <h3 style={titleStyle}> Author: </h3>
+                        <p style={valueStyle}>{activity.author}</p>
+                    </div>
+                    <div style={{...titleAndValueStyle, borderColor: "rgb(255,152,152)"}}>
+                        <h3 style={titleStyle}> Group Size: </h3>
+                        <p style={valueStyle}>{rangeToString(activity.playerCount)} people</p>
+                    </div>
+                    <div style={{...titleAndValueStyle, borderColor: "rgb(170,255,113)"}}>
+                        <h3 style={titleStyle}> Age Range: </h3>
+                        <p style={valueStyle}>{rangeToString(activity.ageRange)} years</p>
+                    </div>
+                    <div style={{...titleAndValueStyle, borderColor: "rgb(49,224,255)"}}>
+                        <h3 style={titleStyle}> Duration: </h3>
+                        <p style={valueStyle}>{rangeToString(activity.duration)} {activity.duration === [1,1] ? "minute" : "minutes"}</p>
+                    </div>
+                    <div style={{...titleAndValueStyle, borderColor: "rgb(100,77,255)"}}>
+                        <h3 style={titleStyle}> Likes: </h3>
+                        <p style={valueStyle}>{activity.likes}</p>
+                    </div>
+                </div>
+                <h2>Description:</h2>
+                <p style={descStyle}>{activity.description}</p>
+                <h2>Images</h2>
+                {/*images go here*/}
             </div>)}
         </div>
     )
