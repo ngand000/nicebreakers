b25c2ed0f6db7f40389edb8e3c6283363969cffb
zylim1128 <zylim@uw.edu>
2024-02-29 14:00:33 -0800

    Updates

--- src/pages/activities/UploadButton.jsx
+++
@@ -1,9 +0,0 @@
-import './UploadButton.css'
-
-// button to go to the upload page
-export default function UploadButton() {
-
-    return (<a href="/upload" className={"uploadButtonFilterStyle"} style={{backgroundColor: "rgb(148, 148, 242)"}}>
-        Upload
-    </a>)
-}
